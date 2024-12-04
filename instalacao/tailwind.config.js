90baa1e6d42b48cba6e9eb92f73d39971faee458
laurabluna <laurablunam@gmail.com>
2024-12-04 11:51:46 -0300

    alterações nesse arquivo

--- instalacao/tailwind.config.js
+++ instalacao/tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    fontFamily: {}
+    extend: {
+      colors: {
+        "azul": '#4998B4',
+        'amarelo': '#FAD55C',
+        "orange": '#ff7849',
+        'green': '#13ce66',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    },
+    fontFamily: {
+      "montserrat" : ['Montserrat', 'sans-serif'],
+    }
   },
   plugins: [],
 }
